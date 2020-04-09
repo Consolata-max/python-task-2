d17cb67cc56c43a8e670c55f6597d46585d137d9
consolata gicheru <52771550+Consolata-max@users.noreply.github.com>
2020-04-10 01:33:14 +0300

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,28 @@
+import string
+import random
+
+x = input('Enter your First name:')
+print('Hello, ' + x)
+
+y = input('Enter your Last name:')
+print('Hello, ' + x + y)
+
+z = input(' Enter your Email Address:')
+print(z)
+
+#password generator
+length = 9
+random.choice(string.ascii_letters)
+password = ''.join (random.choice("(string.ascii_letters") for i in range(length) )
+print (password)
+
+#Password validation
+if (user is satisfied with password)
+print (x + y + z)
+
+else:
+    
+     P = input('Please Enter your Preffered Password:')
+     print( P)
+
+if P ==7
